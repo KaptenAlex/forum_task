5dfd61d568d34719440a5de36564a45a6aa9c7e4
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-11-19 14:03:56 +0100

    Added PostResponsesComponent.jsx

--- components/PostDetailComponents/PostResponsesComponent.jsx
+++ components/PostDetailComponents/PostResponsesComponent.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function PostResponsesComponent() {
+export default function PostResponsesComponent(props) {
     return (
         <div className="col-lg-12">
-            hej3
+            <h1>Post responses</h1>
+            {props.noOfResponses && (
+                <h4>Number of responses on this post: {props.noOfResponses}</h4>
+            )}
+            {props.responses && props.responses.map((response, index) => {
+                console.log(response);
+                return (
+                    <div key={index} className="alert alert-dark">
+                        <h3>{response.author.firstName} {response.author.lastName}</h3>
+                        <h5 className="pl-3">{response.content}</h5>
+                    </div>
+                )
+            })}
         </div>
     )
 }
