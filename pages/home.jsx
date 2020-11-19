63e4461d29034b036313abf6d31316576dfd9c96
KaptenAlex <alexander.m.wilson1996@gmail.com>
2020-11-19 15:07:56 +0100

    Updated styling on home page.

--- pages/home.jsx
+++ pages/home.jsx
@@ -7,15 +7,21 @@ export default function home() {
             <div>
                 <NavbarComponent />
             </div>
-            <h1>Home page!!!</h1>
-            <div>
-                <h2>The WEBB19 Forum</h2>
-                <strong>What is this forum?</strong>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br>
-                    Iste, error. Maxime unde eos, cumque id dignissimos magnam repellendus nam similique officiis saepe nobis, eius alias odio.<br></br>
-                    Perferendis ad amet quaerat?
-                </p>
+            <div className="container">
+                <div className="d-flex justify-content-center">
+                    <div className="col-lg-6">
+                    <h1 className="mb-0">WEBB19</h1>
+                    <h1>Home page</h1>
+                        <div>
+                            <h1>What is this forum?</h1>
+                            <h5>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Iste, error. Maxime unde eos, cumque id dignissimos magnam repellendus nam similique officiis saepe nobis, eius alias odio.
+                                Perferendis ad amet quaerat?
+                            </h5>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
